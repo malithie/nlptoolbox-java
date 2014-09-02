010e47f9acfedae16593ad8610ec659e2c68c56f
Chanuka <chanukadissanayake@ymail.com>
2014-09-02 09:38:45 +0530

    New changelist

---
+++ src/main/java/org/wso2/toolbox/nlp/test/FindNameEntityTypeExecutorTest.java
@@ -0,0 +1,64 @@
+package org.wso2.toolbox.nlp.test;
+
+import org.apache.log4j.Logger;
+import org.wso2.toolbox.nlp.operation.FindNameEntityTypeExecutor;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+/**
+ * Created by Chanuka on 9/1/14 AD.
+ */
+public class FindNameEntityTypeExecutorTest {
+
+    private static Logger logger = Logger.getLogger(DictionaryTest.class);
+    private static long start;
+    private static long end;
+    private static List<String> results;
+    private static List<String> concatenateResults;
+    private static StringBuilder textBuffer = null;
+    private static FindNameEntityTypeExecutor executor = null;
+    public static void main(String[] args) throws Exception {
+        setUp();
+        testFindNameEntityTypeExecutor("PERSON");
+        testFindNameEntityTypeExecutor("LOCATION");
+        testFindNameEntityTypeExecutor("ORGANIZATION");
+    }
+
+    public static void setUp() throws Exception {
+        start = System.currentTimeMillis();
+        executor = new FindNameEntityTypeExecutor();
+        end = System.currentTimeMillis();
+
+        logger.info("Time to construct NameEntityTypeExecutor instance : [" + ((end - start)/1000f) + " sec]");
+
+        textBuffer = new StringBuilder();
+
+        InputStream inputStream = ClassLoader.getSystemResourceAsStream("nameEntityTypeTestFile");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = bufferedReader.readLine()) != null){
+            textBuffer.append(line);
+        }
+
+        inputStream.close();
+        bufferedReader.close();
+
+    }
+
+
+    private static void testFindNameEntityTypeExecutor(String entityType){
+        System.out.println("==================================================================================");
+        start = System.currentTimeMillis();
+        results = executor.findNameEntityType(textBuffer.toString(), entityType);
+        concatenateResults=executor.findNameEntityTypeConcatenate(textBuffer.toString(),entityType);
+
+        end = System.currentTimeMillis();
+        logger.info(String.format("Results of Test: %s", results));
+        logger.info(String.format("Concatenate Results of Test: %s",concatenateResults));
+        System.out.println("-----------------------------------------------------------------------------------");
+        logger.info(String.format("Time to execute FindNameEntityTypeExecutor.findNameEntityType for %s : [%f sec]", entityType, ((end - start)/1000f)));
+
+    }
+}
